fed71d9951fc5b74fc52e6c7665283074c990650
Lorenc326 <iLorenc326@gmail.com>
2022-05-15 21:27:07 +0300

    Update getTweets handler to write res by chunks

--- tweet/repo.go
+++ tweet/repo.go
@@ -2,6 +2,7 @@ package tweet
 
 import (
 	"database/sql"
+	"github.com/Lorenc326/tmitter/db"
 	_ "github.com/go-sql-driver/mysql"
 	"log"
 )
@@ -17,30 +18,34 @@ func (r *repo) getById(id int) (Tweet, error) {
 	return res, err
 }
 
-func (r *repo) getByUser(username string, limit int, offset int) (*[]UserPopulated, error) {
+func (r *repo) getByUser(username string, limit int, offset int) (db.Iterate, db.CloseIterator, error) {
 	rows, err := r.Query(`
 SELECT tweets.id, tweets.createdAt, tweets.username, tweets.text, users.firstName, users.lastName
 FROM tweets 
 LEFT JOIN users USING (username)
 WHERE tweets.username = ?
+ORDER BY tweets.createdAt ASC
 LIMIT ?, ?;
 `, username, offset, limit)
-	defer rows.Close()
 
-	res := []UserPopulated{}
 	if err != nil {
-		return &res, err
+		return nil, nil, err
 	}
 
-	for rows.Next() {
-		var tweet UserPopulated
-		tweet.decode(rows)
-		log.Println(tweet)
-		if err != nil {
-			return &res, err
+	iterator := func() interface{} {
+		if rows.Next() {
+			var tweet UserPopulated
+			tweet.decode(rows)
+			if err != nil {
+				log.Println(err)
+				return nil
+			}
+			return tweet
 		}
-		res = append(res, tweet)
+		return nil
 	}
-
-	return &res, nil
+	closer := func() {
+		rows.Close()
+	}
+	return iterator, closer, nil
 }
